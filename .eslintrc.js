4c976ef39a8547da4228a4dc9044ce2f69e7f293
satyajit.happy <satyajit.happy@gmail.com>
2019-04-28 13:16:02 +0200

    chore: migrate codebase to typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,13 +3,13 @@ module.exports = {
     browser: true,
     es6: true,
   },
-  parser: 'babel-eslint',
   extends: [
     'eslint:recommended',
     'plugin:react/recommended',
     'eslint-config-prettier',
     'eslint-config-prettier/react',
   ],
+  plugins: ['react', 'prettier'],
   settings: {
     react: {
       pragma: 'React',
@@ -23,14 +23,31 @@ module.exports = {
     ecmaVersion: 2018,
     sourceType: 'module',
   },
-  plugins: ['react', 'prettier'],
   rules: {
     'max-len': ['off', 80],
     'no-undef': 'off',
     'linebreak-style': ['error', 'unix'],
-
-    'react/prop-types': 'off',
-
     'prettier/prettier': 'error',
+    'react/prop-types': 'off',
   },
-}
+  overrides: [
+    {
+      files: ['*.js'],
+      parser: 'babel-eslint',
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      plugins: ['@typescript-eslint/eslint-plugin'],
+      rules: {
+        '@typescript-eslint/no-unused-vars': [
+          'error',
+          { argsIgnorePattern: '^_' },
+        ],
+
+        'no-dupe-class-members': 'off',
+        'no-unused-vars': 'off',
+      },
+    },
+  ],
+};
